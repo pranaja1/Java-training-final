95b1ad181c1843604f5bf6077a6fe570a8412338
ABHI <ABHI@ABHI-PC>
2023-07-27 14:22:06 +0530

    commit

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,8 +1,12 @@
+class Assignment{
+public static void main(String[] args){
+ int i = 10 ;
+ int j = 7 ;
+ i = i + j ;
+ j = i - j ;
+ i = i - j ;
+ System.out.println(i);
+System.out.println(j);
+} 
 
-public class Assignment {
-
-	public static void main(String[] args) {
-		System.out.println("Hello2");
-		}
-		}
-
+}
