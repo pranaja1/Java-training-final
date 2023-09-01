dfd280925be2ef4078644de9aefc299ca67c9552
ABHI <ABHI@ABHI-PC>
2023-09-01 12:02:34 +0530

    Commit

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,36 +1,86 @@
 class Assignment{
-public static void main(String[] args){
- int i = 10 ;
- int j = 7 ;
- i = i + j ;
- j = i - j ;
- i = i - j ;
- System.out.println(i);
-System.out.println(j);
+	
+
+	public static void main(String[] args){
+		//int i = 10 ;
+	//	int j = 7 ;
+	//	i = i + j ;
+	//	j = i - j ;
+	//	i = i - j ;
+	//	System.out.println(i);
+		//System.out.println(j);
 
 
-} 
+	
 
-{int i = 10 ;
-int j = 7 ; 
-int k = i ;
- i = j ;
- j = k ;
- System.out.println(i);
-System.out.println(j);
+//	{int i = 10 ;
+//	int j = 7 ; 
+//	int k = i ;
+	//i = j ;
+	//j = k ;
+	//System.out.println(i);
+//	System.out.println(j);
+	//}
+
+
+//	int i = 10 ;
+	//int j = 7 ;
+	//int k = i;
+	//i = j ;
+//	j= k ;
+	
+	
+	//System.out.println(i);
+	
+	//System.out.println(j); 
+	//char ch = 'b'	;
+	//int j = (int)ch ;
+	//System.out.println (j);
+		
+	//	int m = 107 ;
+	//	char ichar = (char) m ;
+	//	System.out.println (ichar);
+		
+		//int i = 10;
+
+		//int j= 7; 
+
+		//double db1 = i/j; 
+		//double db1 = (double)i/j;
+		//double db1 = i/(double)j;
+	//System.out.println (db1);
+
+
+//
+//int i3 = 10;
+//
+//int m = ++i3 + i3++ + i3++ + i3++  + i3-- + i3-- + i3--;
+//
+//System.out.println(m);
+//System.out.println(i3);
+//
+		
+	
+
+	}
 }
+		
+	
 
-{
-{ int i = 10; 
 
-int int = 20; 
 
-int goto = 30; 
 
-int default = 40; 
 
-int class = 50; 
 
-int j = 70;}}
-}
+
+
+
+
+
+
+
+
+
+
+
 
