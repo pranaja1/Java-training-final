4f1b9d3dbe82ee998375bcc0e4d3a2dc802984ce
ABHI <ABHI@ABHI-PC>
2023-09-01 15:35:28 +0530

    COMMIT

---
+++ src/TimeSecondAssignment.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TimeSecondAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scan = new Scanner(System.in);
+		 System.out.println("Input Seconds");
+		 
+		 int Second = scan.nextInt();
+		 
+		 int S = Second % 60;
+		 
+		 System.out.println(S);
+		 int H = Second / 60;
+	        int M = H % 60;
+	        H = H / 60;
+	        System.out.print( H + ":" + M + ":" + S);
+			
+		 
+		 
+		 
+
+	}
+
+}
