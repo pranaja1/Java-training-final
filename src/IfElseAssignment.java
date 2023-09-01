c9b5a9af8be79f596c05645f6e03c375db6a4b53
ABHI <ABHI@ABHI-PC>
2023-09-01 16:09:43 +0530

    commit

--- src/IfElseAssignment.java
+++ src/IfElseAssignment.java
@@ -4,17 +4,38 @@ public class IfElseAssignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		Scanner scan = new Scanner (System.in);
+//		System.out.println("Write the number");
+//		
+//		int i = scan.nextInt();
+//		
+//		if( i < 0) {
+//			System.out.println("Number is Nigative");
+//		}
+//		else{
+//			System.out.println("Number is Positive");
+//		}
+		
 		Scanner scan = new Scanner (System.in);
-		System.out.println("Write the number");
+		System.out.println("Write value for x");
+		int x = scan.nextInt();
 		
-		int i = scan.nextInt();
+		System.out.println("Write value for y");
+		int y = scan.nextInt();
 		
-		if( i < 0) {
-			System.out.println("Number is Nigative");
+		System.out.println("Write value for z");
+		int z = scan.nextInt();
+		
+		if(x>y && x>z) {
+			System.out.println("This is gretest numbure:"+x);
+		} else if (y>z) {System.out.println("This is gretest numbure:"+y);
+		} else {System.out.println("This is gretest numbure:"+z);
+		
 		}
-		else{
-			System.out.println("Number is Positive");
-		}
+		
+		
+		
+		
 		
 
 	}
