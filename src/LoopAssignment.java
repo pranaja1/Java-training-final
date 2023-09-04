0ebd5bf1920bac9bf8f8773137d125404c754da6
ABHI <ABHI@ABHI-PC>
2023-09-04 13:29:31 +0530

    commit

--- src/LoopAssignment.java
+++ src/LoopAssignment.java
@@ -5,20 +5,26 @@ public class LoopAssignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		while (true) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Number");
-		
-		int number = scan.nextInt();
-		int sum = 0;
-		
-		while (number != 0) {
-			sum = sum + number%10 ;
-			number = number/10 ;
+		int i = 1;
+		do {
+			System.out.println("hellow");
+			i++;
 		}
-		System.out.println("Sum of number:"+ sum);
+		while (i<100) ;
+		
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Number");
+//		
+//		int number = scan.nextInt();
+//		int sum = 0;
+//		
+//		while (number != 0) {
+//			sum = sum + number%10 ;
+//			number = number/10 ;
+//		}
+//		System.out.println("Sum of number:"+ sum);
 		
 
-	} }
+ }
 
 }
