7dcab0efb91136bfa96dc50317b18abc761e615d
ABHI <ABHI@ABHI-PC>
2023-09-12 16:50:22 +0530

    commit

--- src/ClassAssignment.java
+++ src/ClassAssignment.java
@@ -1,9 +1,51 @@
 
-public class ClassAssignment {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+
+
+class Human {
+	String s = "object human";
+			
+			public static void main (String args[]) {
+				
+				Human a = new Human ();
+				System.out.println(a.s);
+			}}
+
+class Laptop {
+	int i = 10;
+
+	
+	public static void main (String args[]) {
+		
+		Laptop s = new Laptop ();
+		
 	}
-
+	
+		
 }
+
+class Sportsman {
+	int i = 10;
+
+	
+	public static void main (String args[]) {
+		
+		Sportsman s = new Sportsman ();
+		
+	}}
+
+class Testcase {
+	int i = 10;
+
+	
+	public static void main (String args[]) {
+		
+		Testcase s = new Testcase ();
+		
+	}}
+
+
+	
+	
